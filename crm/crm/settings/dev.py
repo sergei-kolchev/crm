defd598b45257ed0f04bfe14ab7f382b032b7ba1
sergei-kolchev <s.kolchev@me.com>
2024-04-02 23:48:55 +0200

    bug fixes

--- crm/crm/settings/dev.py
+++ crm/crm/settings/dev.py
@@ -10,7 +10,7 @@ INTERNAL_IPS = ["127.0.0.1"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DATABASE"),
+        "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
--- crm/crm/settings/prod.py
+++ crm/crm/settings/prod.py
@@ -7,7 +7,7 @@ DEBUG = False
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DATABASE"),
+        "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
